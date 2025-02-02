0fae28364d521fcfd727d42447330dd3b3828ebe
alwinbhandari <alwinbhandari303@gmail.com>
2025-02-02 09:41:56 +0545

    elwin

---
+++ .vscode/strtedjs/calculator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b,c;
+    cout<<"enter your number"<<endl;
+    cin>>a;
+    b=a*1.6;
+    c=b+((10/100)*b);
+
+
+
+
+
+    cout<<"bharu : "<<b<<endl;
+    cout<<"with 10% : "<<b<<endl;
+    return 0;
+}
+
